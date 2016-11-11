71704d9de42c317fe3ebfd4dd316cf66b9f030c9
tosgit1989 <purannaa1989@gmail.com>
2016-11-11 17:08:38 +0900

    パスを修正

--- reviews/new.php
+++ reviews/new.php
@@ -2,11 +2,12 @@
 require_once ('../app.php');
 $HotelId = $methods->getHotelId($_SERVER['REQUEST_URI']);
 $ReviewId = $methods->getReviewId($_SERVER['REQUEST_URI']);
+echo $ReviewId;
 ?>
 <html>
 <body>
 レビューの新規作成
-<form method="POST" action="<?php echo sprintf('/reviews/save.php/%s', $ReviewId) ?>">
+<form method="POST" action="<?php echo sprintf('/reviews/exec.php/%s', $ReviewId) ?>">
     <div class="form-group">
         <input required="required" class="form-control" placeholder="レートを入力" name="rate" type="text">
     </div>
