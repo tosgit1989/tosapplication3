765d4d3538f54255c9215e4ca8e59c011a0a31d2
tosgit1989 <purannaa1989@gmail.com>
2016-11-11 16:36:57 +0900

    レビュー作成ページに入力フォームを作成

--- reviews/new.php
+++ reviews/new.php
@@ -4,5 +4,15 @@ require_once ('../app.php');
 <html>
 <body>
 レビューの新規作成
+<form method="POST" action="<?php echo sprintf('/reviews/save.php/%s', $ReviewId) ?>">
+    <div class="form-group">
+        <input required="required" class="form-control" placeholder="レートを入力" name="rate" type="text">
+    </div>
+    <div class="form-group">
+        <input required="required" class="form-control" placeholder="レビューを入力" name="review" type="text">
+    </div>
+    <button type="submit">投稿する</button>
+</form>
+
 </body>
 </html>
