6d97dd43ac3b8b7f22590caa2a96115298b1683c
tosgit1989 <purannaa1989@gmail.com>
2016-11-12 07:33:23 +0900

    revert

--- reviews/new.php
+++ reviews/new.php
@@ -4,5 +4,15 @@ require_once ('../app.php');
 <html>
 <body>
 レビューの新規作成
+<form method="POST" action="/reviews/exec.php/h=<?php echo $HotelId ?>_r=new_u=<?php echo $UserId ?>">
+    <div class="form-group">
+        <input required="required" class="form-control" placeholder="レートを入力" name="rate" type="text">
+    </div>
+    <div class="form-group">
+        <input required="required" class="form-control" placeholder="レビューを入力" name="review" type="text">
+    </div>
+    <button type="submit">投稿する</button>
+</form>
+
 </body>
 </html>
