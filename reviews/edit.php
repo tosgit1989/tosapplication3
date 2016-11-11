6adf237790d93d7549a4494c7303efca05c9e3d0
tosgit1989 <purannaa1989@gmail.com>
2016-11-12 00:42:50 +0900

    レビュー編集ページに入力フォームを作成

--- reviews/edit.php
+++ reviews/edit.php
@@ -1,8 +1,18 @@
 <?php
 require_once ('../app.php');
+$review = $dataConnect->findReview($ReviewId);
 ?>
 <html>
 <body>
 レビューの編集
+<form method="POST" action="/reviews/exec.php/h=<?php echo $HotelId ?>_r=<?php echo $ReviewId ?>_u=<?php echo $UserId ?>">
+    <div class="form-group">
+        <input required="required" class="form-control" placeholder="レートを入力" name="rate" type="text" value="<?php $review['rate'] ?>">
+    </div>
+    <div class="form-group">
+        <input required="required" class="form-control" placeholder="レビューを入力" name="review" type="text" value="<?php $review['review'] ?>">
+    </div>
+    <button type="submit">更新する</button>
+</form>
 </body>
 </html>
