877a2f2bfe14212b05e1a2f418150f4639d8ed84
tosgit1989 <purannaa1989@gmail.com>
2016-11-11 10:44:48 +0900

    トップページに新着ホテル一覧が表示されるようにした。

--- hotels/index.php
+++ hotels/index.php
@@ -1,10 +1,25 @@
 <?php
 require_once ('../src/Services/DataHandler.php');
 require_once ('../src/Services/Methods.php');
+$data_connect = new \Services\DataHandler();
+$hotels = $data_connect->all();
 ?>
 <html>
 <body>
 トップページ
+<?php
+foreach ($hotels as $hotel) {
+    echo $hotel['hotel_name'];
+    echo $hotel['image_url'];
+    echo $hotel['detail'];
+    echo $hotel['access'];
+    echo $hotel['address'];
+    echo $hotel['fee1'];
+    echo $hotel['fee2'];
+    echo $hotel['created_at'];
+    echo $hotel['updated_at'];
+}
+?>
 </body>
 </html>
 
--- src/Services/DataHandler.php
+++ src/Services/DataHandler.php
@@ -1,2 +1,22 @@
 <?php
+namespace Services;
+class DataHandler {
+    // get_pdo()
+    protected function get_pdo() {
+        $db_connect = 'mysql:dbname=tosapplication_development; host=127.0.0.1; charset=utf8';
+        $user_name = 'root';
+        $password = '';
+        $driver_options = array();
+        $pdo = new \PDO($db_connect, $user_name, $password, $driver_options);
+        return $pdo;
+    }
+
+    // hotel_all()
+    public function all() {
+        $pdo = $this->get_pdo();
+        $hotel_query = $pdo->prepare('SELECT * FROM hotels order by updated_at desc');
+        $hotel_query->execute();
+        return $hotel_query->fetchAll();
+    }
+}
 ?>
