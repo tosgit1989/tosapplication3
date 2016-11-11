2817ba6bdf5ae96ea0c4029a4ffe682dbb59e60e
tosgit1989 <purannaa1989@gmail.com>
2016-11-12 06:17:12 +0900

    レビュー作成ページへのリンクの書き方を変更

--- hotels/show.php
+++ hotels/show.php
@@ -15,7 +15,11 @@ echo $hotel['fee1'];
 echo $hotel['fee2'];
 echo $hotel['created_at'];
 echo $hotel['updated_at'];
-echo sprintf('<a href="/reviews/new.php/h=%s_r=new_u=0">このホテルのレビューを書く</a>', $hotel['id']);
+echo '<a href="/reviews/new.php/h=';
+echo $hotel['id'];
+echo '_r=new_u=';
+echo $UserId;
+echo '">このホテルのレビューを書く</a>';
 ?>
 <p>みんなのレビュー</p>
 <?php
