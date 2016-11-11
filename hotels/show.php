f5147ab428a274f055e479cff53297bbb3a490d4
tosgit1989 <purannaa1989@gmail.com>
2016-11-11 23:23:39 +0900

    ホテル詳細ページにて、そのホテルへのレビューが表示されるようにした

--- hotels/show.php
+++ hotels/show.php
@@ -1,6 +1,7 @@
 <?php
 require_once ('../app.php');
 $hotel = $dataConnect->findHotel($HotelId);
+$reviews = $dataConnect->getReviewAll();
 ?>
 <html>
 <body>
@@ -15,6 +16,18 @@ echo $hotel['fee2'];
 echo $hotel['created_at'];
 echo $hotel['updated_at'];
 echo sprintf('<a href="/reviews/new.php/h=%s_r=new_u=0">このホテルのレビューを書く</a>', $hotel['id']);
+?>
+<p>みんなのレビュー</p>
+<?php
+foreach ($reviews as $review) {
+    if ($review['hotel_id'] == $HotelId) {
+        echo $review['rate'];
+        echo $review['review'];
+        echo $review['created_at'];
+        echo $review['updated_at'];
+    }
+}
+
 ?>
 </body>
 </html>
