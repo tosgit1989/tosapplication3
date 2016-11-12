dbef6916bf0ae52eaa1ca4fa27a367c12cae7223
tosgit1989 <purannaa1989@gmail.com>
2016-11-12 09:42:18 +0900

    検索機能の実装に向け、if文の条件を変更

--- src/Services/Methods.php
+++ src/Services/Methods.php
@@ -4,7 +4,7 @@ class Methods{
     // getEachId($request)
     public function getEachId($request) {
         //$request='/reviews/edit.php/h=1_r=1_u=1'
-        if ($request == '/hotels/index.php') {
+        if ($request == '/hotels/index.php' or in_array('"hotels/search.php"', $request)) {
             $idArray = array('h=0', 'r=0', 'u=0');
         } else {
             $idArray = explode('_', array_pop(explode('/', $request)));
