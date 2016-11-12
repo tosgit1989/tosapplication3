ca3f42290745c57f3a2dcaf9224956890ac7f2c3
tosgit1989 <purannaa1989@gmail.com>
2016-11-12 13:03:41 +0900

    app.phpにbootstrapのCSSを読み込む記述を追記

--- app.php
+++ app.php
@@ -8,5 +8,8 @@ $HotelId = array_pop(explode('=', $idArray[0]));
 $ReviewId = array_pop(explode('=', $idArray[1]));
 $UserId = array_pop(explode('=', $idArray[2]));
 ?>
+<head>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+</head>
 <div><a href="/hotels/index.php">hotelication</a></div>
 <div><a href="/hotels/search.php/p=_h=_d=_">ホテルを検索</a></div>
