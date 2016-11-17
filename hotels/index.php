110db120ba428a8e02eae766f3b2cfb4a4ab6824
tosgit1989 <purannaa1989@gmail.com>
2016-11-17 11:03:30 +0900

    一覧最下部とフッターとの間に隙間を設けた。

--- hotels/index.php
+++ hotels/index.php
@@ -12,35 +12,36 @@ $hotels = $dataConnect->getAll('hotels');
         <div class="col-xs-12">
             <div class="bs-docs-section">
 
-<h3 class="text-middle">新着ホテル</h3>
-<?php
-$t = 0;
-foreach ($hotels as $hotel) {
-    $t += 1;
-    if ($t <= 20) {
-        echo '<div class="media">';
-        echo '<div class="col-xs-12 col-sm-5 col-md-4 col-lg-3">';
-        echo sprintf('<a href="/hotels/show.php/%s">', $hotel['id']);
-        echo '<img class="media-object" src="';
-        echo $hotel['image_url'];
-        echo '" alt="hotel_picture" style="width: 100%; height: auto">';
-        echo '</a>';
-        echo '</div>';
-        echo '<div class="col-xs-12 col-sm-7 col-md-8 col-lg-9">';
-        echo '<h4 class="media-heading">';
-        echo sprintf('<a href="/hotels/show.php/%s">', $hotel['id']);
-        echo $hotel['hotel_name'];
-        echo '</a>';
-        echo '</h4>';
-        echo $hotel['detail'];
-        echo '</br>';
-        echo sprintf('<a href="/reviews/new.php/hotelId=%s">このホテルのレビューを書く</a>', $hotel['id']);
-        echo sprintf('<a href="/hotels/show.php/%s">このホテルのレビュー・詳細を見る</a>', $hotel['id']);
-        echo '</div>';
-        echo '</div>';
-    }
-}
-?>
+                <h3 class="text-middle">新着ホテル</h3>
+                <?php
+                $t = 0;
+                foreach ($hotels as $hotel) {
+                    $t += 1;
+                    if ($t <= 20) {
+                        echo '<div class="media">';
+                        echo '<div class="col-xs-12 col-sm-5 col-md-4 col-lg-3">';
+                        echo sprintf('<a href="/hotels/show.php/%s">', $hotel['id']);
+                        echo '<img class="media-object" src="';
+                        echo $hotel['image_url'];
+                        echo '" alt="hotel_picture" style="width: 100%; height: auto">';
+                        echo '</a>';
+                        echo '</div>';
+                        echo '<div class="col-xs-12 col-sm-7 col-md-8 col-lg-9">';
+                        echo '<h4 class="media-heading">';
+                        echo sprintf('<a href="/hotels/show.php/%s">', $hotel['id']);
+                        echo $hotel['hotel_name'];
+                        echo '</a>';
+                        echo '</h4>';
+                        echo $hotel['detail'];
+                        echo '</br>';
+                        echo sprintf('<a href="/reviews/new.php/hotelId=%s">このホテルのレビューを書く</a>', $hotel['id']);
+                        echo sprintf('<a href="/hotels/show.php/%s">このホテルのレビュー・詳細を見る</a>', $hotel['id']);
+                        echo '</div>';
+                        echo '</div>';
+                    }
+                }
+                ?>
+                <div class="media"></div>
 
             </div>
         </div>
--- hotels/search.php
+++ hotels/search.php
@@ -13,45 +13,46 @@ $hotels = $dataConnect->getAll('hotels');
             <div class="bs-docs-section">
 
                 <h3 class="text-middle">検索</h3>
-<form method="POST" action="/hotels/search.php">
-    <div class="form-group">
-        <input class="form-control" placeholder="都道府県を入力" name="prefecture" type="text" value="<?php $_POST['prefecture'] ?>"><br>
-        <input class="form-control" placeholder="ホテル名を入力" name="hotel_name" type="text" value="<?php $_POST['hotel_name'] ?>"><br>
-        <input class="form-control" placeholder="詳細情報を入力" name="detail" type="text" value="<?php $_POST['detail'] ?>"><br>
-    </div>
-    <button class="btn btn-primary" type="submit">検索</button>
-</form>
+                <form method="POST" action="/hotels/search.php">
+                    <div class="form-group">
+                        <input class="form-control" placeholder="都道府県を入力" name="prefecture" type="text" value="<?php $_POST['prefecture'] ?>"><br>
+                        <input class="form-control" placeholder="ホテル名を入力" name="hotel_name" type="text" value="<?php $_POST['hotel_name'] ?>"><br>
+                        <input class="form-control" placeholder="詳細情報を入力" name="detail" type="text" value="<?php $_POST['detail'] ?>"><br>
+                    </div>
+                    <button class="btn btn-primary" type="submit">検索</button>
+                </form>
 
-<h3>検索結果</h3>
-<?php
-foreach ($hotels as $hotel) {
-    $A = $methods->matchKeyword($hotel['address'], $_POST['prefecture']);
-    $B = $methods->matchKeyword($hotel['hotel_name'], $_POST['hotel_name']);
-    $C = $methods->matchKeyword($hotel['detail'], $_POST['detail']);
-    if ($A >= 0 and $B  >= 0 and $C >= 0 and abs($A) + abs($B) + abs($C) >= 1) {
-        echo '<div class="media">';
-        echo '<div class="col-xs-12 col-sm-5 col-md-4 col-lg-3">';
-        echo sprintf('<a href="/hotels/show.php/%s">', $hotel['id']);
-        echo '<img class="media-object" src="';
-        echo $hotel['image_url'];
-        echo '" alt="hotel_picture" style="width: 100%; height: auto">';
-        echo '</a>';
-        echo '</div>';
-        echo '<div class="col-xs-12 col-sm-7 col-md-8 col-lg-9">';
-        echo '<h4 class="media-heading">';
-        echo sprintf('<a href="/hotels/show.php/%s">', $hotel['id']);
-        echo $hotel['hotel_name'];
-        echo '</a>';
-        echo '</h4>';
-        echo $hotel['detail'];
-        echo '</br>';
-        echo sprintf('<a href="/reviews/new.php/hotelId=%s">このホテルのレビューを書く</a>', $hotel['id']);
-        echo sprintf('<a href="/hotels/show.php/%s">このホテルのレビュー・詳細を見る</a>', $hotel['id']);
-        echo '</div>';
-        echo '</div>';
-    }
-}
-?>
+                <h3>検索結果</h3>
+                <?php
+                foreach ($hotels as $hotel) {
+                    $A = $methods->matchKeyword($hotel['address'], $_POST['prefecture']);
+                    $B = $methods->matchKeyword($hotel['hotel_name'], $_POST['hotel_name']);
+                    $C = $methods->matchKeyword($hotel['detail'], $_POST['detail']);
+                    if ($A >= 0 and $B  >= 0 and $C >= 0 and abs($A) + abs($B) + abs($C) >= 1) {
+                        echo '<div class="media">';
+                        echo '<div class="col-xs-12 col-sm-5 col-md-4 col-lg-3">';
+                        echo sprintf('<a href="/hotels/show.php/%s">', $hotel['id']);
+                        echo '<img class="media-object" src="';
+                        echo $hotel['image_url'];
+                        echo '" alt="hotel_picture" style="width: 100%; height: auto">';
+                        echo '</a>';
+                        echo '</div>';
+                        echo '<div class="col-xs-12 col-sm-7 col-md-8 col-lg-9">';
+                        echo '<h4 class="media-heading">';
+                        echo sprintf('<a href="/hotels/show.php/%s">', $hotel['id']);
+                        echo $hotel['hotel_name'];
+                        echo '</a>';
+                        echo '</h4>';
+                        echo $hotel['detail'];
+                        echo '</br>';
+                        echo sprintf('<a href="/reviews/new.php/hotelId=%s">このホテルのレビューを書く</a>', $hotel['id']);
+                        echo sprintf('<a href="/hotels/show.php/%s">このホテルのレビュー・詳細を見る</a>', $hotel['id']);
+                        echo '</div>';
+                        echo '</div>';
+                    }
+                }
+                ?>
+                <div class="media"></div>
 
             </div>
         </div>
