e5a594801650c1cf2c810eac20e859abda0cf2ad
tosgit1989 <purannaa1989@gmail.com>
2016-11-17 17:16:55 +0900

    サインインした際の、「PHP Notice: Undefined index...」が出ないようにした。

--- users/session.php
+++ users/session.php
@@ -1,19 +1,21 @@
 <?php
 require_once ('../app.php');
-$users = $dataConnect->getAll('users');
-$p = 0;
-foreach ($users as $user) {
-    if ($_POST['email'] == $user['email'] and $_POST['psw'] == $user['psw']) {
-        $p += 1;
+if (isset($_POST['email']) and isset($_POST['psw'])) {
+    $users = $dataConnect->getAll('users');
+    $p = 0;
+    foreach ($users as $user) {
+        if ($_POST['email'] == $user['email'] and $_POST['psw'] == $user['psw']) {
+            $p += 1;
+        }
     }
-}
-if($p == 1) {
-    if(isset($_POST['email'])) setcookie("email", $_POST['email'], time()+120);
-    $findUserByEmail = $dataConnect->findUserByEmail($_POST['email']);
-    $_SESSION['id'] = $findUserByEmail['id'];
-    header('Location: /hotels/top_page.php');
-} else {
-    $_SESSION['id'] = null;
-    header('Location: /users/sign_in.php');
+    if($p == 1) {
+        if(isset($_POST['email'])) setcookie("email", $_POST['email'], time()+120);
+        $findUserByEmail = $dataConnect->findUserByEmail($_POST['email']);
+        $_SESSION['id'] = $findUserByEmail['id'];
+        header('Location: /hotels/top_page.php');
+    } else {
+        $_SESSION['id'] = null;
+        header('Location: /users/sign_in.php');
+    }
 }
 ?>
