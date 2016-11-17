5eace83a70988c3041d75feeab2c673baa53c8e3
tosgit1989 <purannaa1989@gmail.com>
2016-11-17 12:44:25 +0900

    タグ名修正

--- reviews/new.php
+++ reviews/new.php
@@ -13,7 +13,7 @@ $hotel = $dataConnect->findById($HotelId, 'hotels');
             <div class="bs-docs-section">
 
                 <!--投稿対象のホテル-->
-                <divr class="media">
+                <div class="media">
                     <div class="col-xs-12 col-sm-5 col-md-4 col-lg-3">
                         <a href="/hotels/show.php/<?php echo $hotel['id'] ?>">
                             <img class="media-object" src="<?php echo $hotel['image_url'] ?>" alt="hotel_picture" style="width: 100%; height: auto">
