4e084358ad3f4afb0a1312f53abdf535d32367c0
tosgit1989 <purannaa1989@gmail.com>
2016-11-25 08:51:33 +0900

    if文内の条件の書き方を変更

--- src/Services/Methods.php
+++ src/Services/Methods.php
@@ -7,7 +7,7 @@ class Methods{
         if (preg_match('"hotels/show.php"', $request)) {
             //ホテル詳細ページにいる時
             $hId = array_pop(explode('/', $request));
-        } elseif (preg_match('"reviews/new.php"', $request) or preg_match('"hotelId="', $request)) {
+        } elseif (preg_match('"reviews"', $request) and preg_match('"hotelId="', $request)) {
             //レビュー新規作成時
             $hId = array_pop(explode('=', array_pop(explode('/', $request))));
         }
@@ -17,10 +17,10 @@ class Methods{
     //getReviewId($request)
     public function getReviewId($request) {
         $rId = 0;
-        if (preg_match('"reviews/new.php"', $request) or preg_match('"hotelId="', $request)) {
+        if (preg_match('"reviews"', $request) and preg_match('"hotelId="', $request)) {
             //レビュー新規作成時
             $rId = 'new';
-        } elseif (preg_match('"reviews"', $request) or preg_match('"exec.php"', $request)) {
+        } elseif (preg_match('"reviews"', $request) and !preg_match('"hotelId="', $request)) {
             //レビュー編集時または削除時
             $rId = array_pop(explode('/', $request));
         }
