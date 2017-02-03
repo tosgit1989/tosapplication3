18760a4e57c15b647b6f49c430f4d5a548ce13d8
tosgit1989 <purannaa1989@gmail.com>
2017-02-04 05:26:24 +0900

    ユーザー更新時にメッセージが表示されるようにした。

--- users/exec.php
+++ users/exec.php
@@ -5,5 +5,21 @@ $userUpd['nickname'] = $_POST['nickname'];
 $userUpd['email'] = $_POST['email'];
 $userUpd['updated_at'] = $now = date('Y/m/d H:i:s');
 $dataConnect->update($userUpd, ['id' => $UserId], 'users');
-header('Location: /index.php');
 ?>
+
+<div class="page-title">
+    <p class="page-title-text">
+        ユーザー情報を更新しました。
+    </p>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="bs-docs-section">
+
+                <a href="/users/show.php/<?php echo $UserId ?>" class="btn btn-primary" role="button">マイページへ</a>
+
+            </div>
+        </div>
+    </div>
+</div>
