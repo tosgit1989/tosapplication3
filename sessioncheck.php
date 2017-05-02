621b8b7887b8d60e26ce6f93a96a90fb7e0bf729
tosgit1989 <purannaa1989@gmail.com>
2017-05-02 17:02:37 +0900

    コードの構成変更

--- sessioncheck.php
+++ sessioncheck.php
@@ -1,16 +1,15 @@
 <?php
-
 if (preg_match('"users/message.php"', $_SERVER['REQUEST_URI'])
     or preg_match('"users/sign_in.php"', $_SERVER['REQUEST_URI'])
     or preg_match('"users/sign_up.php"', $_SERVER['REQUEST_URI'])) {
-    // 上記4通りの場合は、セッションが効いていなくても何もしない
-} elseif ($_SESSION['id'] < 1) {
-    // 上記の場合以外で、セッションが効いていない場合
-    header('Location: /users/message.php/sessionTimeOut');
+    // 1. 何もしない
 } else {
-    //セッションが効いている場合
-    $userId = $_SESSION['id'];
-    $user = $dataConnect->getById($userId, 'users');
+    // 2. セッションが無効ならサインインページへリダイレクト、有効ならユーザーIDによりユーザー情報を取得
+    if ($_SESSION['id'] < 1) {
+        header('Location: /users/message.php/sessionTimeOut');
+    } else {
+        $userId = $_SESSION['id'];
+        $user = $dataConnect->getById($userId, 'users');
+    }
 }
-
 ?>
